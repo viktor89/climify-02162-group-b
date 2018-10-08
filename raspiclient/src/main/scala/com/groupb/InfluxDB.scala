93234ad8da3216b2bc508d4bfd4eb50840a399ae
Per Lange Laursen <perlangelaursen@gmail.com>
2018-10-08 08:51:38 +0200

    InfluxDB simplifications

--- raspiclient/src/main/scala/com/groupb/InfluxDB.scala
+++ raspiclient/src/main/scala/com/groupb/InfluxDB.scala
@@ -8,41 +8,29 @@ import scala.util._
 import scala.collection.JavaConversions._
 import com.paulgoldbaum.influxdbclient._
 
-class Data(_sensor : String, _time : Int, _value : Int) {
+class Data(_sensor : String, _time : Any, _value : Any) {
   val sensorName: String = _sensor
-  val time: Int = _time
-  val value: Int = _value
+  val time: Any = _time
+  val value: Any = _value
 }
 
 object InfluxDBHandler {
   val influxdb = InfluxDB.connect("localhost", 8086, "openhab", "AnotherSuperbPassword456-")
   val database = influxdb.selectDatabase("openhab_db")
 
-  var latest = 0
-
   def readData(): String = {
     val list = new ArrayList[Data]
-    val measurementQuery = database.query("SELECT * FROM /^*/")
-    val result = Await.result(measurementQuery, Duration.Inf)
+    val seriesQuery = database.query("SELECT * FROM /^*/")
+    val result = Await.result(seriesQuery, Duration.Inf)
     result.series.foreach(serie =>
       serie.records.foreach(record =>
         list.add(new Data(serie.name,
-          record("time").asInstanceOf[Int],
-          record("value").asInstanceOf[Int]))))
-
-    latest = 0
-    for(point <- list) {
-      if (point.time > latest) {
-        latest = point.time
-      }
-    }
-
+          record("time"),record("value")))))
     JsonMapper.toJson(list)
   }
 
   def clearDB(): Unit = {
-    val timestamp = Integer.toString(latest)
-    val clearQuery : Future[QueryResult] = database.query("DELETE WHERE time <= " + timestamp)
+    val clearQuery = database.query("DELETE WHERE time > 2018-06-19")
     Await.result(clearQuery, Duration.Inf)
   }
 }
