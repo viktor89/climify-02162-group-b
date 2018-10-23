78657929e614cb66bdc5214716a5a616facf56e0
Per Lange Laursen <perlangelaursen@gmail.com>
2018-10-23 18:46:25 +0200

    Refactored readData to use flatMap and map instead of using a mutable Java ArrayList

--- raspiclient/src/main/scala/com/groupb/InfluxDB.scala
+++ raspiclient/src/main/scala/com/groupb/InfluxDB.scala
@@ -1,22 +1,17 @@
 package com.groupb
 
-import java.util.ArrayList
 import scala.concurrent.duration.Duration
 import scala.concurrent.{ Await, Future }
-import scala.util._
-import scala.collection.JavaConverters
 import com.paulgoldbaum.influxdbclient._
 import com.paulgoldbaum.influxdbclient.Parameter.Precision.Precision
 
 object InfluxDBHandler {
   def readData(db : Database) = {
-    val list = new ArrayList[Data]
     val seriesQuery = db.query("SELECT * FROM /^*/", Parameter.Precision.SECONDS)
     val result = Await.result(seriesQuery, Duration.Inf)
-    result.series.foreach(serie =>
-      serie.records.foreach(record =>
-        list.add(Data(serie.name, record("time"),record("value")))))
-    JavaConverters.asScalaBuffer(list).toSeq
+    result.series.flatMap(serie =>
+      serie.records.map(record =>
+        Data(serie.name, record("time"), record("value"))))
   }
 
   def clearDB(db : Database)(data : Seq[Data]) = {
