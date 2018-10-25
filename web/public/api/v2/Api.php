5c42673343742af86c3eb1cb857785d5079d695e
Viktor Poulsen <viktor@maigaard.io>
2018-10-25 19:33:45 +0200

    fixed float conversion

--- web/public/api/v2/Api.php
+++ web/public/api/v2/Api.php
@@ -23,7 +23,7 @@ class Api
     }
 
     public function validateMeasurement($measurement) {
-        $measurement->value = floatval($measurement->value, 2);
+        $measurement->value = sprintf("%.2f", $measurement->value);
         if(!is_float($measurement->value)){throw new \Exception('Measurement value was not a float');}
         if(empty($measurement->sensorName)){throw new \Exception('Sensor name wasn\'t a string');}
         if(!$this->isValidTimeStamp($measurement->time)){throw new \Exception('Timestamp not valid');}
