94a665d8113cad24d3ab1881bcb069588ea305af
Viktor Poulsen <viktor@maigaard.io>
2018-10-31 13:26:47 +0100

    various fixes

--- web/public/ink/views/view-devices.php
+++ web/public/ink/views/view-devices.php
@@ -8,9 +8,9 @@
         <span>
             <span>
                 <?php if ( $currentUserRole == 1 ) { ?>
-                <select class="list-schools-other-users" name="option">
+                    <select class="list-schools-other-users" name="option">
                     <option value="" selected>Choose Institution</option>
-                    <!-- Content goes here -->
+                        <!-- Content goes here -->
                 </select>
                 <?php } ?>
                 <button id="btn-show-all-decices" class="link"></button>	
@@ -30,11 +30,49 @@
     </div> <!-- list-all-devices-wrapper -->
     <h4 class="map-selection-head map-selection-head-edit"><!-- Content goes here --></h4>
     <div class="map-wrapper map-wrapper-admin">
-        <iframe src="http://grafana.se2-webapp02.compute.dtu.dk/d-solo/fN_AzXbik/skoleklima?refresh=5s&orgId=1&panelId=2&from=1540879984159&to=1540901584159" width="100%" height="500" frameborder="0"></iframe>
+        <div class="map-location">
+            <p class="canvasDataInfo map-info map-info-no-school">Choose a school to see a floor plan</p>
+            <p class="canvasDataInfo map-info map-info-no-map">No floor plans available</p>
+            <div class="map-frame-wrapper">
+                <div class="map-frame map-frame-edit">
+                    <!-- Content goes here -->
+                </div>
+                <div id="device-frame-edit" class="device-frame device-frame-edit">
+                    <!-- Content goes here -->
+                </div>
+            </div>
+        </div>
+        <div class="map-settings">
+            <select class="map-selection map-selection-edit" name="option">
+                <!-- Content goes here -->
+            </select>
+            <div class="add-remove-map-wrapper">
+                <button class="btn-show-upload-new-map">Add new floor plan</button>
+                <button class="btn-delete-selected-map">Delete chosen map</button>
+            </div>
+            <div class="upload-map-selection">
+                <p>For the best result, use a picture of at least 800px x 500px.<br>You can use pictures in the format jpg, png og gif.</p>
+                <form id="frm-map-upload" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="inp-map-user-role" id="inp-map-user-role" value="<?php echo $currentUserRole ?>">
+                    <input type="hidden" name="inp-map-school" id="inp-map-school" value="<?php  ?>">
+                    <input type="text" name="inp-map-name" id="inp-map-name" placeholder="Navngiv kort">
+                    <div class="upload-map-label-wrapper">
+                        <label for="upload-map-file" class="upload-map-label">Choose floor plan from drive
+                            <input type="file" name="upload-map-file" id="upload-map-file" accept="image/x-png,image/gif,image/jpeg" >
+                        </label>
+                    </div>
+                </form>
+            </div>
+            <h4>Available sensors:</h4>
+            <div id="place-devices-list">
+                <!-- content goes here -->
+            </div>
+            <button id="btn-save-devices-placement">Save changes</button>
+        </div>
     </div>
     <div class="crop-img-admin">
         <div class= "progress-align">
-            
+
             <div class="header-progress-container">
                 <a href="#/" id="close-cropper" onclick="resetUpload()" ><i class="ico-close-signup link fa fa-times"></i></a>
                 <ol class="header-progress-list">
