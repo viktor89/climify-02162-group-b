3c464c093681b94aea9054adbda2949c961a861f
Viktor Poulsen <viktor@maigaard.io>
2018-10-31 14:08:50 +0100

    removed var dump

--- web/public/api/api-get-influx-info.php
+++ web/public/api/api-get-influx-info.php
@@ -20,7 +20,6 @@ if( $apiPassword !== $phase_api_key){
     exit;
 }
 $returnFromIfx = file_get_contents("http://".getenv('INFLUXDB_HOST').":8086/query?u=".getenv('MYSQL_USER')."&p=".getenv('MYSQL_PASSWORD')."&db=".getenv('MYSQL_DATABASE')."&q=SHOW%20MEASUREMENTS%20limit%201");
-var_dump($returnFromIfx);
 $returnFromIfx = json_decode($returnFromIfx, true);
 
 if ($returnFromIfx[results][0][series][0][name] != "measurements") {
