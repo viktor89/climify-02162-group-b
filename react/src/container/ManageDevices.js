c2cddb4fc997246623bf8ce6eaf0b5b908340bd7
Malamin Bah <malamin@malamins-mbp.eduroam.wireless.dtu.dk>
2018-11-06 14:49:31 +0100

    device2.0

---
+++ react/src/container/ManageDevices.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import GetSensorInstance from "../component/GetSensorInstance";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing.unit * 2
+  }
+});
+
+class ManageDevice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { getSensor: [] };
+  }
+  componentWillMount() {
+    axios
+      .get("/api/v2/sensor/getData.php")
+      .then(response => {
+        this.setState(() => {
+          return { getSensor: response.data };
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    const { classes } = this.props;
+    const {
+      getSensor: []
+    } = this.state;
+
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={6}>
+          <h3>Devices</h3>
+          <GetSensorInstance hubs={getSensor} />
+        </Grid>
+      </Grid>
+    );
+  }
+}
+export default withStyles(styles)(ManageDevice);
