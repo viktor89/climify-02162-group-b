bd8bc982611e21c90788b5cb50036744a567a132
Viktor Poulsen <viktor@maigaard.io>
2018-11-06 15:28:01 +0100

    reset DAO

--- web/public/api/v2/sensor/SensorDAO.php
+++ web/public/api/v2/sensor/SensorDAO.php
@@ -7,58 +7,33 @@ use InfluxDB\Point;
 class SensorDAO extends API\V2\Api
 {
     /**
-     * @param $mac
-     * @return false|string
+     * @param $json
+     * @throws ValidationException
      * @throws Exception
      */
-    
-    
-    //  public function writeDataAsPoints($json){
-    //     $points = [];
-    //     if(empty($json)){
-    //         throw new Exception('Unable to parse json');
-    //     }
-    //     if(empty($json->mac)){
-    //         throw new ValidationException('No mac address provided!');
-    //     }
+    public function writeDataAsPoints($json){
+         $points = [];
+         if(empty($json)){
+             throw new Exception('Unable to parse json');
+         }
+         if(empty($json->mac)){
+             throw new ValidationException('No mac address provided!');
+         }
 
-    //     foreach($json->data as $measurement){
-    //         $this->validator::validateMeasurement($measurement);
-    //         $points[] =
-    //             new Point(
-    //                 'sensor_measurements', // name of the table
-    //                 (float) sprintf("%.2f", $measurement->value), // the measurement value
-    //                 ['sensor_name' => $measurement->sensorName, 'hubID' => $json->mac, "sensor_type" => $measurement->sensorType],
-    //                 [],
-    //                 $measurement->time // Time precision has to be set to seconds!
-    //             );
-    //     }
+         foreach($json->data as $measurement){
+             $this->validator::validateMeasurement($measurement);
+             $points[] =
+                 new Point(
+                     'sensor_measurements', // name of the table
+                     (float) sprintf("%.2f", $measurement->value), // the measurement value
+                     ['sensor_name' => $measurement->sensorName, 'hubID' => $json->mac, "sensor_type" => $measurement->sensorType],
+                     [],
+                     $measurement->time // Time precision has to be set to seconds!
+                 );
+         }
 
-        
-
-
-    //     if(!$this->influxDb->writePoints($points)){
-    //         throw new Exception('Unexpected error while writing data points');
-    //     }
-    // }
-
-
-    public function getSensorInstance(){
-        $statement = $this->database->prepare("SELECT SensorID, SensorTypeID, HubID, RoomName,BuildingID FROM Room NATURAL JOIN SensorInstance ");
-
-        $statement->execute();
-        $statement->store_result();
-        $statement->bind_result($hubMac);
-
-        $hubs = [];
-        /* fetch values */
-        while ($statement->fetch()) {
-            $hubs[] = ["PiID" => $hubMac, "building" => $building, "room" => $room, "SensorType"=>$stype, "SensorID" => $sID ];
-        }
-        $statement->close();
-
-        echo json_encode($hubs);
-    }
-    
-    
+         if(!$this->influxDb->writePoints($points)){
+             throw new Exception('Unexpected error while writing data points');
+         }
+     }
 }
