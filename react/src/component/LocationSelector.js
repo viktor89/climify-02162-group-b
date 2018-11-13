270c055d11d488990e0d43633fdb25771f157fed
Viktor Poulsen <viktor@maigaard.io>
2018-11-13 20:21:48 +0100

    refactor

--- react/src/component/LocationSelector.js
+++ react/src/component/LocationSelector.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import TreeSelect, {SHOW_PARENT} from 'rc-tree-select';
+import TreeSelect, {SHOW_ALL, SHOW_PARENT} from 'rc-tree-select';
 import 'rc-tree-select/assets/index.css';
 import './LocationSelector.css';
+import axios from "axios";
 
 const styles = theme => ({
   treeSelect: {
@@ -17,29 +18,33 @@ class LocationSelector extends Component {
     this.state = {
       tsOpen: false,
       visible: false,
-      searchValue: '0-0-0-label',
-      // value: ['0-0-0-0-value', '0-0-0-1-value', '0-0-0-2-value'],
-      lv: {value: '0-0-0-value', label: 'spe label'},
       multipleValue: [],
-      simpleSearchValue: 'test111',
-      simpleTreeData: [
-        {key: 1, pId: 0, label: 'test1', value: 'test1'},
-        {key: 121, pId: 0, label: 'test2', value: 'test2'},
-        {key: 11, pId: 1, label: 'test11', value: 'test11'},
-        {key: 12, pId: 1, label: 'test12', value: 'test12'},
-        {key: 111, pId: 11, label: 'test111', value: 'test111'},
-      ],
-      treeDataSimpleMode: {
-        id: 'key',
-        rootPId: 0,
-      },
+      buildings: []
     }
     this.onClick = this.onClick.bind(this);
     this.onChange = this.onChange.bind(this);
-    this.onChangeChildren = this.onChangeChildren.bind(this);
-    this.onMultipleChange = this.onMultipleChange.bind(this);
-    this.onSelect = this.onSelect.bind(this);
-    this.onDropdownVisibleChange = this.onDropdownVisibleChange.bind(this);
+    this.getBuildings = this.getBuildings.bind(this);
+  }
+
+  componentWillMount(){
+    this.getBuildings();
+  }
+
+  getBuildings() {
+    axios
+      .get("/api/v2/institution/getBuildings.php")
+      .then(response => {
+        this.setState(() => {
+          return { buildings: response.data.map((building => ({
+              value: building.name,
+              title: building.name,
+              children: building.rooms.map((room) => ({
+                value: room.hubID,
+                title: room.roomName,
+              }))
+            }))) };
+        });
+      })
   }
 
   onClick() {
@@ -48,57 +53,27 @@ class LocationSelector extends Component {
     });
   }
 
-  onChange(value, ...rest) {
-    console.log('onChange', value, ...rest);
+  onChange(value, label, extra) {
+    console.log(extra);
     this.setState({ value });
   }
 
-  onChangeChildren(...args) {
-    console.log('onChangeChildren', ...args);
-    const value = args[0];
-    const pre = value ? this.state.value : undefined;
-    this.setState({ value: isLeaf(value) ? value : pre });
-  }
-
-  onMultipleChange(value) {
-    console.log('onMultipleChange', arguments);
-    this.setState({ multipleValue: value });
-  }
-
-  onSelect() {
-    // use onChange instead
-    console.log(arguments);
-  }
-
-  onDropdownVisibleChange(visible, info) {
-    console.log(visible, this.state.value, info);
-    return true;
-  }
-
   render() {
+    const { buildings } = this.state;
     return (
       <TreeSelect
         transitionName="rc-tree-select-dropdown-slide-up"
         choiceTransitionName="rc-tree-select-selection__choice-zoom"
-        dropdownStyle={{ height: 200, overflow: 'auto' }}
         dropdownPopupAlign={{ overflow: { adjustY: 0, adjustX: 0 }, offset: [0, 2] }}
-        onDropdownVisibleChange={this.onDropdownVisibleChange}
         placeholder={<i>Select Buildings and rooms here</i>}
         searchPlaceholder="please search"
         treeLine maxTagTextLength={10}
         value={this.state.value}
         autoClearSearchValue
-        treeData={[
-          {value: 0, title: '0', children: [
-              {value: 1, title: '11'},
-              {value: 12, title: '12'},
-            ]},
-          {value: 2, title: '2'}
-        ]}
+        treeData={ buildings }
         treeNodeFilterProp="title"
         treeCheckable showCheckedStrategy={SHOW_PARENT}
         onChange={this.onChange}
-        onSelect={this.onSelect}
         maxTagCount={20}
         maxTagPlaceholder={(valueList) => {
           console.log('Max Tag Rest Value:', valueList);
