97c5d082cdb2213244168c33a4515c711be3be4b
Unknown <spirem@gmail.com>
2018-11-21 18:35:18 +0100

    getUsers email instead of lastlogin

--- web/public/api/v2/users/UserDAO.php
+++ web/public/api/v2/users/UserDAO.php
@@ -7,16 +7,16 @@ use API\V2\ValidationException;
 class UserDAO extends API\V2\Api
 {
     public function getUsers(){
-        $statement = $this->database->prepare("SELECT UserID, UserName, FirstName, LastName, Role.RoleName, LastLogin FROM Person LEFT JOIN Role ON Person.RoleName = Role.RoleID");
+        $statement = $this->database->prepare("SELECT UserID, UserName, FirstName, LastName, Email, Role.RoleName FROM Person LEFT JOIN Role ON Person.RoleName = Role.RoleID");
 
         $statement->execute();
         $statement->store_result();
-        $statement->bind_result($userID, $userName, $firstName, $lastName, $role, $lastLogin);
+        $statement->bind_result($userID, $userName, $firstName, $lastName, $email, $role);
 
         $users = [];
         /* fetch values */
         while ($statement->fetch()) {
-            $users[] = ["id" => $userID, "username" => $userName, "firstname" => $firstName, "lastname" => $lastName, "role" => $role, "lastLogin" => $lastLogin];
+            $users[] = ["id" => $userID, "username" => $userName, "firstname" => $firstName, "lastname" => $lastName, "email" => $email, "role" => $role];
         }
         $statement->close();
         return $users;
