97782474ffa50e75c584d4003d237652aa865b32
Viktor Poulsen <viktor@maigaard.io>
2018-11-23 13:41:56 +0100

    added running status

--- web/public/api/v2/sensor/SensorDAO.php
+++ web/public/api/v2/sensor/SensorDAO.php
@@ -72,7 +72,7 @@ class SensorDAO extends API\V2\Api
         $sensors = [];
         /* fetch values */
         while ($statement->fetch()) {
-            $sensors[] = ["HubID" => $hubMac, "Building" => $building, "Room" => $room, "SensorType"=>$stype, "SensorID" => $sID ];
+            $sensors[] = ["HubID" => $hubMac, "Building" => $building, "Room" => $room, "SensorType"=>$stype, "SensorID" => $sID, "running" => count($this->influxDb->getDataSeries($sID, 5)) > 0 ];
         }
         $statement->close();
         return $sensors;
