eec4d416cc9efdd65d5cabd1d15b5ec33ae644ab
Per Lange Laursen <perlangelaursen@gmail.com>
2018-11-23 22:34:57 +0100

    Slight change to TransmissionActor

--- raspiclient/src/main/scala/com/groupb/TransmissionActor.scala
+++ raspiclient/src/main/scala/com/groupb/TransmissionActor.scala
@@ -1,12 +1,10 @@
 package com.groupb
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorLogging, Props}
 import akka.event.Logging
 import com.paulgoldbaum.influxdbclient._
 
-class TransmissionActor(val transmitter : Transmission) extends Actor {
-  private val log = Logging(context.system, this)
-
+class TransmissionActor(val transmitter : Transmission) extends Actor with ActorLogging {
   def receive = {
     case "send" => {
       log.info("Data transmission started")
