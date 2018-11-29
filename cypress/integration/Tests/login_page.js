4d91850fd15e4e1b288b577d46f2ccb1ec4e7228
Malamin Bah <malamin@Malamins-MacBook-Pro.local>
2018-11-29 23:20:13 +0100

    a

--- cypress/integration/Tests/login_page.js
+++ cypress/integration/Tests/login_page.js
@@ -1,24 +1,12 @@
-/// <reference types="Cypress" />
 context("Actions", () => {
   beforeEach(function() {
     // reset and seed the database prior to every test
     // cy.exec("npm run db:reset && npm run db:seed");
-    cy.request("POST", "http://localhost:8000", { username: "ben" })
-      .its("body")
-      .as("currentUser");
+    cy.visit("http://localhost:8000/");
   });
-  it("successfully loads login pssage", function() {
-    cy.visit("http://localhost:8000/#/sensors"); // change URL to match your dev URL
-  });
-
-  it("succsesfully logs in", function() {
-    // https://on.cypress.io/type
-    cy.get("deviceList")
-      .type("fake@email.com")
-      .should("have.value", "fake@email.com");
-  });
-
-  it("Unsuccessfully loads the page", function() {
-    cy.visit("http://localhost:8000/#/sensors"); // change URL to match your dev URL
+  it("Succesfully logs in", function() {
+    cy.get("#inp-login-name").type("admin");
+    cy.get("#inp-login-pass").type("admin");
+    cy.get("#btn-system-login").click();
   });
 });
