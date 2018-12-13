110c8cf3527c9dced499e95fc36c1e6e92c505ec
Malamin Bah <malamin@malamins-mbp.eduroam.wireless.dtu.dk>
2018-12-13 11:05:48 +0100

    cypressUpdate

--- cypress/integration/Tests/login_page.js
+++ cypress/integration/Tests/login_page.js
@@ -1,7 +1,6 @@
-context("Actions", () => {
+context("Log in", () => {
   beforeEach(function() {
-    // reset and seed the database prior to every test
-    // cy.exec("npm run db:reset && npm run db:seed");
+    
     cy.visit("http://localhost:8000/");
   });
   it("Succesfully logs in", function() {
